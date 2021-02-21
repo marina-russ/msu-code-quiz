feb44864fad28823bcc871ceddc187c8c5171c3f
Marina Russ <marina@cameystudios.com>
2021-02-21 14:30:53 -0500

    adds timer

--- assets/script.js
+++ assets/script.js
@@ -3,16 +3,13 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 
 const questionCounterText = document.getElementById('question-counter');
 const scoreCounterText = document.getElementById('score-counter');
+const startButton = document.getElementById('start-btn');
 
 const answerPoints = 10;
 const maxTestQuestions = 5;
 
-const startButton = document.getElementById('start-btn');
-const showTimer = document.getElementById('timer-div');
-
 let currentQuestion = {};
 let questionCounter = 0;
-let scoreCounter = 0;
 let availableQuestions = [];
 // questions and answers are pulled from https://www.codeconquest.com/coding-quizzes/javascript-knowledge-quiz-beginner/
 let questions = [
@@ -67,65 +64,91 @@ let questions = [
     answer: 3
   }
 
-]
+];
 
-// arrow function: function name = (parameters) => return
-startGame = () => {
-  //! startTimer();
+const startingMinutes = 1;
+let time = (startingMinutes * 30) - 1; //to start at 2:59 instead of 3:00, due to unknown time delay bug
+const countdownEl = document.getElementById('countdown');
+let timerInterval;
 
+// * START GAME, START TIMER, SHOW QUESTIONS
+startButton.addEventListener('click', function(){
+  // ? COUNTDOWN TIMER
+  // NOTE: there is a delay on timer start
+  // clears interval ID if it is already set
+  // prevents timer from having seizures when startButton is clicked after the first time
+  if (timerInterval != null) {
+    clearInterval(timerInterval);
+    timerInterval = null;
+  } else {
+    timerInterval = setInterval(updateCountdown, 1000);
+  }
+
+  // timer math
+  function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    // conditional statement to prevent timer from starting at 3:0 instead of 3:00, 
+    seconds = seconds < 10 ? ('0' + seconds) : seconds;
+    // display countdown as text on page
+    countdownEl.innerHTML = `${minutes}:${seconds}`;
+    time--;
+
+    // ? END QUIZ IF TIMER RUNS OUT
+    // time == -1 because when we let (time = startingMinutes) near line 70 we subtracted 1 
+    if (time == -1) {
+      alert("time is up!");
+      return window.location.assign("index.html");
+    }
+  }
+
+  // ? REVEAL QUESTION/ANSWER SECTION ON QUIZ START
+  let qaContainer = document.getElementById("qa-container");
+    console.log(qaContainer);
+    console.log(qaContainer.style.display);
+
+    if (qaContainer.style.display == "none") {
+      qaContainer.style.display = "block";
+      console.log("show this");
+    } else if (qaContainer.style.display == "") {
+      qaContainer.style.display = "block";
+      console.log("secret secret");
+    } else {
+      qaContainer.style.display = "none";
+      console.log("hide this");
+    }
+  console.log(qaContainer.style.display);
+
+  // ? QUIZ GAME MECHANICS
   questionCounter = 0;
   score = 0;
-
-  // ... spread syntax 'clones' the QUESTIONS array into this array, so any changes made here don't also change that array
+  // '...' (spread) syntax 'clones' the QUESTIONS array into this array, so any changes made here don't also change that array
   availableQuestions = [...questions];
-  //console.log(availableQuestions);
+
   getNewQuestion();
-};
-
-// TODO: change <div id="time-div"> class name CSS from 'hidden' to 'display'
-//! function startTimer() {
-//   var presentTime = document.getElementById('timer-counter').innerHTML;
-//   var timeArray = presentTime.split(/[:]+/);
-//   var m = timeArray[0];
-//   var s = checkSecond((timeArray[1] - 1));
-//   if (s == 59) { m = m - 1 }
-//   //if(m<0){alert('timer completed')}
-
-//   document.getElementById('timer').innerHTML =
-//     m + ":" + s;
-//   console.log(m)
-//   setTimeout(startTimer, 1000);
-
-//   showTimer.addEventListener('click', function (
-
-//     let timerText = document.getElementById('timer-counter');
-
-//   document.getElementById('timer-counter').innerHTML = 1 + ":" + 00;
-
-// ));
-
-//}
-
-
+});
 
 // * UPDATING SCORE
 incrementScore = num => {
   score += num;
   scoreCounterText.innerText = score;
-}
+};
 
+decreaseTime = num => {
+  time -=10;
+  scoreCounterText.innerText = score;
+};
+
+// * SHOWING NEW QUESTION
 getNewQuestion = () => {
 
   if (availableQuestions.length === 0) {
-    //if (availableQuestions.length === 0 || timer = 0) {} 
     // save score to local storage
     //localStorage.setItem("mostRecentScore", score);
-
     localStorage.setItem("mostRecentScore", JSON.stringify(score));
-
     // move to winner.html page
     return window.location.assign("winner.html");
-  }
+  } 
 
   // update HUD question counter each round
   questionCounter++;
@@ -140,12 +163,12 @@ getNewQuestion = () => {
 
   // choices is the link to <p> choice-text. forEach combined with dataset updates every <p> with the appropriate answer.
   choices.forEach(choice => {
-    const number = choice.dataset['number'];
+    const number = choice.dataset.number;
     choice.innerText = currentQuestion['choice' + number];
   });
 
   // splice current aQ from qI so that the question is not repeated, and qI shrinks each round
-  availableQuestions.splice(questionIndex, 1)
+  availableQuestions.splice(questionIndex, 1);
   //console.log(availableQuestions);
 };
 
@@ -153,11 +176,10 @@ getNewQuestion = () => {
 choices.forEach(choice => {
   choice.addEventListener('click', event => {
     const selectedChoice = event.target;
-      //console.log(event.target);
-    const selectedAnswer = selectedChoice.dataset['number'];
+    const selectedAnswer = selectedChoice.dataset.number;
       //console.log(selectedAnswer == currentQuestion.answer);
 
-    // add correct(green) or incorrect(red) class styling. 
+    // add correct (green) or incorrect (red) class styling. 
     // ternary operator (either/or):
     const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
@@ -165,6 +187,11 @@ choices.forEach(choice => {
     if (classToApply === 'correct') {
       incrementScore(answerPoints);
     }
+
+    if (classToApply === 'incorrect') {
+      decreaseTime(answerPoints);
+    }
+
     // add appropriate class styling
     selectedChoice.parentElement.classList.add(classToApply);
     // give small delay before removing class and showing a new question
@@ -175,7 +202,3 @@ choices.forEach(choice => {
 
   });
 });
-
-startGame();
-
-//********************** */
